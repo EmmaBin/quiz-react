909690ec2f5a2d522d0211d918e1496ffa3c8cba
emmacodes <binma.emma@gmail.com>
2023-01-21 16:45:38 -0800

    styled questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import { useState } from 'react'
 
 export default function Question(props){
-    // I need to shuffle the incorrect answer and correct answer and only display with original order once in a component lifecycle
+    // shuffle the incorrect answer and correct answer and only display with original order once in a component lifecycle
     // set initial state as a callback function will help me achieve this effect
-    const [answers, setAnswers] = useState(()=> setAllAnswers(props.correctAnswer, props.incorrectAnswers))
+    const [answers, setAnswers] = useState(()=> shuffleAllAnswers(props.correctAnswer, props.incorrectAnswers))
 
     function decodeHtml(question) {
         var txt = document.createElement('textarea');
@@ -12,22 +12,21 @@ export default function Question(props){
         return txt.value;
       }
 
-    function setAllAnswers(correctAnswer, incorrectAnswers){
+    function shuffleAllAnswers(correctAnswer, incorrectAnswers){
         const randomPosition = Math.floor(Math.random()*3)
         correctAnswer = decodeHtml(correctAnswer)
-
         let answerList = incorrectAnswers.map(answer => decodeHtml(answer))
-      
         answerList.splice(randomPosition, 0, correctAnswer)
 
         return answerList
-
     }
+
     return(
         <div className='question-container'>
             {/*'company&#039;s logo is the &#039;' HTML entities displayed on the screen, decode function will help*/}
             <h2 className='question'>{decodeHtml(props.question)}</h2>
-            <h5 className='answers'>{answers}</h5>
+            {answers.map(answer => <button className='answer--btn' id={props.id}>{answer}</button>)}
+            <br></br>
         </div>
     )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -7,10 +7,17 @@ import Question from './Question'
 export default function Quiz(){
 // when the page first time loaded, two things I want to achieve->1. store the questions array 2. build Question component
 
-// document user's click(input), after click on check 'check for answer' btn, all five must be chosen
+// document user's click(input), after click on check 'check for answer' btn, all five must be chosen, if not disable the check for answer btn 
 // display all correct choice(different color),
 // check for answer btn will be also change to 'new game' btn and display the score x/5 correct
-    const [questions, setQuestions] = useState([])
+
+//选什么，对不对
+// one state user answer
+    const [questions, setQuestions] = useState([]) 
+    // const [answers, setAnswers] = useState([]) [a1, a2, a3]
+
+
+
     
     useEffect(()=>{
         fetch('https://opentdb.com/api.php?amount=5&type=multiple')
@@ -20,14 +27,23 @@ export default function Quiz(){
     },[])
 
     return(
+        <div className='quiz-component'>
         <div className='quiz--page'>
-        {questions.map((result) => 
+        {questions.map((result, index) => 
            <Question question={result.question} 
                     key={nanoid()}
                     incorrectAnswers={result.incorrect_answers}
                     correctAnswer={result.correct_answer} 
+                    id={nanoid()}
+                    // userAnswer={answers[index]}
                     />
         )}
+
+        <div className='check--container'>
+        <button className='score--btn'>Check answers</button>
+        </div>
+        
+        </div>
         </div>
     )
 }
