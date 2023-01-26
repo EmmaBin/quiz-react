8586773ef9d3596f3b355c335e40a340b8eeb266
emmacodes <binma.emma@gmail.com>
2023-01-25 22:58:56 -0800

    readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Quiz in _React_
+## Play the game @ https://lambent-starlight-872cbf.netlify.app/
+### Features
+- Using Quizzical Trivia API
+- useState()
+- useEffect()
+- conditional rendering
+- findIndex(), filter(), map(), splice()methods
+
+![alt text](./src/assets/image.png)
+
+
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -6,7 +6,7 @@ export default function Welcome(props){
             <h1 className='welcome--title'>
                 Quizzical
             </h1>
-            <h4>Let's have some fun, see how many you can score!</h4>
+            <h4>Let's have some fun, see how many points you can score!</h4>
             <button className='welcome--btn' onClick={props.handleClick}>Start Game</button>
         </div>
     )
