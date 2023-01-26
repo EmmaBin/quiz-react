e5b2938010f992de2018b608b996301401c50008
emmacodes <binma.emma@gmail.com>
2023-01-25 17:56:05 -0800

    new game worked but lost disable btn functionality

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,6 +5,7 @@ export default function Question(props){
     // shuffle the incorrect answer and correct answer and only display with original order once in a component lifecycle
     // set initial state as a callback function will help me achieve this effect
     const [answers, setAnswers] = useState(()=> shuffleAllAnswers(props.correctAnswer, props.incorrectAnswers))
+    
 
     function decodeHtml(question) {
         var txt = document.createElement('textarea');
@@ -21,13 +22,15 @@ export default function Question(props){
         return answerList
     }
 
-
+    // const styles ={backgroundColor: props.clickColor ? "#F8BCBC" : 'transparent'}
     return(
         <div className='question-container'>
             {/*'company&#039;s logo is the &#039;' HTML entities displayed on the screen, decode function will help*/}
             <h2 className='question'>{decodeHtml(props.question)}</h2>
-            {answers.map(answer => <button className='answer--btn' 
+            {answers.map(answer => <button 
+                                           className='answer--btn' 
                                            onClick = {() => props.handleClick(props.question, answer)}
+                                           
                                            key= {answer}
                                         
                                            >{answer}</button>)}
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -17,7 +17,11 @@ export default function Quiz(){
     const [questions, setQuestions] = useState([]) 
     const [answers, setAnswers] = useState([])
     const [showScore, setShowScore]= useState(false)
-    const readyToScore= answers.filter(answer => typeof answer!= 'undefined').length === questions.length 
+    //when new game btn being clicked, false => true, when it's true, disable new game btn, disable score displaying, 
+    //show the reveal score btn
+    const [newGame, setNewGame] = useState(false)
+    let readyToScore= answers.filter(answer => typeof answer!= 'undefined').length === questions.length 
+    // const [clickColor, setClickColor] = useState(false)
     
 
     function handleQuestionClick(questionStr, answerStr){
@@ -33,8 +37,7 @@ export default function Quiz(){
         console.log(answers)
         return(
             setAnswers(answerCopy)
-            
-            
+           
         )
     }
     
@@ -43,18 +46,28 @@ export default function Quiz(){
         .then(data => data.json())
         .then(res => setQuestions(res.results)
         )  
-    },[])
+        // console.log(newGame)
+    },[newGame])
 
 
     function checkScore(){
         let correctAnswer = 0
         for (let i=0; i<answers.length; i++){
             if (answers[i]===questions[i].correct_answer){
+
                 correctAnswer++
             }
         }
         return correctAnswer
         
+    }
+
+    function startNewGame(){
+        setNewGame(pre => !pre)
+        setShowScore(false)
+        readyToScore = false
+        console.log(readyToScore)
+
     }
     return(
         <div className='quiz-component'>
@@ -66,14 +79,19 @@ export default function Quiz(){
                     correctAnswer={result.correct_answer} 
                     // selectedAnswer={/* answer string  from `answers` */}
                     handleClick={handleQuestionClick}
+                    
+                    
                     />
         )}
 
         <div className='check--container'>
+        
             
         {!showScore && <button className='score--btn' onClick={() =>setShowScore(true)} disabled={!readyToScore}>Check answers</button>}
+        
         {showScore && `You scored ${checkScore()}/5 correct answers`}
-        {showScore && <button className='new-game-btn'>Start new game</button>}
+        {showScore && <button className='new-game-btn' onClick={startNewGame}>Start new game</button>}
+        
        
         </div>
         
