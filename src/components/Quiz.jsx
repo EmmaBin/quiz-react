8a18ba7368945d009c1d4505b7f24e25645ac5ae
emmacodes <binma.emma@gmail.com>
2023-02-13 20:03:40 -0800

    editted comments

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import { useEffect } from 'react'
 import { useState } from 'react'
-import {nanoid} from 'nanoid'
 import Question from './Question'
 
 export default function Quiz(){
 // when the page first time loaded, two things I want to achieve->1. store the questions array 2. build Question component
 
-// after user chooses the answer:1. btn color changed, need to document user's pick, 
-//                               2. after five questions answered, enable the check answer btn and tally the answer
-//                               3. display correct answer by showing a color and display the correct number of the answer
-//                               4. display 'start new game' btn, 
+// after user chooses all questions:1. btn color changed, need to document user's pick, 
+//                                  2. after five questions answered, enable the check answer btn and tally the answer
+//                                  3. display correct answer by showing a different color and display the number of the correct answer
+//                                  4. display 'start new game' btn, 
 
 
 
@@ -20,10 +19,10 @@ export default function Quiz(){
     //when new game btn being clicked, false => true, when it's true, disable new game btn, disable score displaying, 
     //show the reveal score btn
     const [newGame, setNewGame] = useState(false)
+    //readyToScore is to determine the btn disable functionality 
     let readyToScore= answers.filter(answer => typeof answer!= 'undefined').length === questions.length 
     
     
-
     function handleQuestionClick(questionStr, answerStr){
         //create a answer array copy with the new answer every time, so react will rerender 
         //corresponding answer to its question, checking with questions arr, they are pair arr
@@ -42,12 +41,13 @@ export default function Quiz(){
         )
     }
     
+    // save an arr of objects inside [questions]
     useEffect(()=>{
         fetch('https://opentdb.com/api.php?amount=5&type=multiple')
         .then(data => data.json())
         .then(res => setQuestions(res.results)
         )  
-        // console.log(newGame)
+        
     },[newGame])
 
 
@@ -63,6 +63,7 @@ export default function Quiz(){
         
     }
 
+
     function startNewGame(){
         setNewGame(pre => !pre)
         setShowScore(false)
@@ -76,18 +77,15 @@ export default function Quiz(){
                     key={result.question}
                     incorrectAnswers={result.incorrect_answers}
                     correctAnswer={result.correct_answer} 
-                    // answer arr 里存了所有的选择的str
+                    // answers arr saved all the answerStr, index is the index number from questions arr which is the same as in answers arr
                     selectedAnswer={answers[index]}
                     handleClick={handleQuestionClick}
                     showScore={showScore}
-                    
-                    
                     />
         )}
 
         <div className='check--container'>
         
-        {/* if setShowScore is true, change the background color for all correct answer */}
         {!showScore && <button className='score--btn' onClick={() =>setShowScore(true)} disabled={!readyToScore}>Check answers</button>}
         
         {showScore && `You scored ${checkScore()}/5 correct answers`}
